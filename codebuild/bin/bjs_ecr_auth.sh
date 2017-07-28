f166606fc4a45edf8f4f9cf4f9adc57363441512
Pahud Hsieh <pahudnet@gmail.com>
2017-07-28 09:42:58 +0800

    update base64 parameter

--- codebuild/bin/bjs_ecr_auth.sh
+++ codebuild/bin/bjs_ecr_auth.sh
@@ -3,12 +3,12 @@
 # retrieve BJS credentials for ECR pusher
 
 
-echo $ak_kms_cipherblob | base64 -D > ak.blob
+echo $ak_kms_cipherblob | base64 -d > ak.blob
 
 ak=$(aws --region us-west-2 kms decrypt --ciphertext-blob fileb://ak.blob --output text --query Plaintext | base64 --decode)
 
 
-echo $sk_kms_cipherblob | base64 -D > sk.blob
+echo $sk_kms_cipherblob | base64 -d > sk.blob
 
 sk=$(aws --region us-west-2 kms decrypt --ciphertext-blob fileb://sk.blob --output text --query Plaintext | base64 --decode)
 
